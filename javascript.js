416ad119b5e5cf54994c3fb5e35ab7091a312dac
Jared Hernandez <herjaredsinai@gmail.com>
2022-03-14 18:30:10 -0400

    Create createGrid function

--- javascript.js
+++ javascript.js
@@ -1,17 +1,23 @@
 const container = document.querySelector(".box");
 
 
-/** Creates a 16x16 grid */
-for (let i=0;i<16;i++){
-    const gridColumn = document.createElement('div');
-    gridColumn.classList.add("gridColumn");
-    container.append(gridColumn);
+/** createGrid creates a grid based on user input */
+
+function createGrid(){
     for (let i=0;i<16;i++){
-        const gridSquare = document.createElement("div");
-        gridSquare.classList.add("gridSquare");
-        gridColumn.appendChild(gridSquare);
+        const gridColumn = document.createElement('div');
+        gridColumn.classList.add("gridColumn");
+        container.append(gridColumn);
+        for (let i=0;i<16;i++){
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("gridSquare");
+            gridColumn.appendChild(gridSquare);
+        }
     }
 }
+
+/**Create event listeners so that when a user hovers over a square it will
+ * change color */
 
 const squares = document.querySelectorAll(".gridSquare");
 squares.forEach(singleSquare => singleSquare.addEventListener('mouseover',()=>{
