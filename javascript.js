d6e0fc78fd8f6f9fe516baa667692bfb33affb48
Jared Hernandez <herjaredsinai@gmail.com>
2022-03-14 18:39:33 -0400

    Flex squares into rows instead of columns

--- javascript.js
+++ javascript.js
@@ -4,18 +4,18 @@ const container = document.querySelector(".box");
 /** createGrid creates a grid based on user input */
 
 function createGrid(){
-    for (let i=0;i<16;i++){
+    for (let i=0;i<32;i++){
         const gridColumn = document.createElement('div');
         gridColumn.classList.add("gridColumn");
         container.append(gridColumn);
-        for (let i=0;i<16;i++){
+        for (let i=0;i<32;i++){
             const gridSquare = document.createElement("div");
             gridSquare.classList.add("gridSquare");
             gridColumn.appendChild(gridSquare);
         }
     }
 }
-
+createGrid();
 /**Create event listeners so that when a user hovers over a square it will
  * change color */
 
