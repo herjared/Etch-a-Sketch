faa95d83070b70ca185afe2a5132d83dd553e4db
Jared Hernandez <herjaredsinai@gmail.com>
2022-03-15 16:10:25 -0400

    Create setGrid with appropiate button in html

--- javascript.js
+++ javascript.js
@@ -4,33 +4,45 @@ const container = document.querySelector(".box");
 
 /** createGrid creates a grid based on user input */
 
-function createGrid(){
+function createGrid(userChoice = 16){
+
     /**Create grid columns */
-    for (let i=0;i<10;i++){
+    for (let i=0;i<userChoice;i++){
         const gridColumn = document.createElement('div');
         gridColumn.classList.add("gridColumn");
         container.append(gridColumn);
         
         /**Create grid squares for columns */
-        for (let i=0;i<10;i++){
+        for (let i=0;i<userChoice;i++){
             const gridSquare = document.createElement("div");
             gridSquare.classList.add("gridSquare");
             gridColumn.appendChild(gridSquare);
         }
     }
+    addListeners();
 }
-
 createGrid();
 
+function deleteGrid(){
+    
+}
 /**Create event listeners so that when a user hovers over a square it will
  * change color */
 
-const squares = document.querySelectorAll(".gridSquare");
-squares.forEach(singleSquare => singleSquare.addEventListener('mousemove',()=>{
-    singleSquare.setAttribute('style', 'background-color:red');
-}));
+function addListeners(){
+    const squares = document.querySelectorAll(".gridSquare");
+    squares.forEach(singleSquare => singleSquare.addEventListener('mousemove',()=>{
+        singleSquare.setAttribute('style', 'background-color:red');
+    }));
+}
+
 
 /**clearGrid returns grid back to its original color */
 function clearGrid(){
+    const squares = document.querySelectorAll(".gridSquare");
     squares.forEach(singleSquare=>singleSquare.setAttribute('style', 'background-color:black'));
+}
+function setGrid(){
+    let userInput = prompt("How many squares on each side?");
+    createGrid(userInput);
 }
